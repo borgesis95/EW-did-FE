111709ba35c2a6572c00fba8b63a8153f2817bc6
Salvatore <borgesisalvo@gmail.com>
2023-04-01 17:21:51 +0200

    add registration page

---
+++ pages/register.tsx
@@ -0,0 +1,85 @@
+import Head from 'next/head';
+import { Button, FormControl, styled, TextField } from '@mui/material';
+import { Box, Container } from '@mui/system';
+import { useEffect, useState } from 'react';
+import { createUserApi, RegistrationUser } from '@/api/user';
+import { initWeb3 } from './login';
+import Web3 from 'web3';
+import { redirect } from 'next/dist/server/api-utils';
+import { useRouter } from 'next/router';
+
+let web3: Web3 | undefined = undefined; // Will hold the web3 instance
+
+function Register() {
+  const [body, setBody] = useState<RegistrationUser>();
+  const router = useRouter();
+
+  useEffect(() => {}, []);
+
+  const handleChange = (key: string, value: string) => {
+    setBody({
+      ...body,
+      [key]: value
+    });
+  };
+
+  const handleSignIn = async () => {
+    if (!web3) {
+      web3 = await initWeb3();
+    }
+
+    const publicAddress = await web3.eth.getCoinbase();
+
+    createUserApi(publicAddress, body).then((response) => {
+      router.push('/login');
+    });
+  };
+
+  return (
+    <>
+      <div>
+        <Head>
+          <title>Sign in </title>
+        </Head>
+        <div className="flex justify-center items-center p-14">
+          <Container maxWidth="sm">
+            <FormControl fullWidth>
+              <TextField
+                id="username"
+                label="Username"
+                variant="outlined"
+                onChange={(event) =>
+                  handleChange('username', event.currentTarget.value)
+                }
+              />
+              <TextField
+                style={{ marginTop: 8 }}
+                id="email"
+                label="Email"
+                variant="outlined"
+                InputLabelProps={{
+                  shrink: true
+                }}
+                onChange={(event) =>
+                  handleChange('email', event.currentTarget.value)
+                }
+              />
+
+              <div className="flex justify-end mt-4">
+                <Button
+                  variant="contained"
+                  style={{ width: '30%' }}
+                  onClick={handleSignIn}
+                >
+                  Sign in
+                </Button>
+              </div>
+            </FormControl>
+          </Container>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Register;
